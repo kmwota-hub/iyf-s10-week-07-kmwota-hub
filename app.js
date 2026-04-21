216fa3541ec1fca379467d49bffc0919d1a024ac
Mwotsam <kmwota@gmail.com>
2026-04-21 14:28:10 +0300

    Change Laptop image URL to GitHub link
    Updated image URL for Laptop product to point to GitHub.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const state = {
     products: [
-        { id: 1, name: "Laptop", price: 999, image: "images/laptop.png" },
+        { id: 1, name: "Laptop", price: 999, image: "https://github.com/kmwota-hub/iyf-s10-week-07-kmwota-hub/blob/2ae669c474742b1c09c4982aac281406a15f1a88/images/Laptop.png" },
         { id: 2, name: "Phone", price: 699, image: "images/phone.png" },
         { id: 3, name: "Headphones", price: 199, image: "images/headphones.png" }
     ],
